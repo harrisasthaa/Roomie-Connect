61dcb0e37f5509f2000353950fd06c8dac53b8e4
cplagge2 <cplagge@wisc.edu>
2023-03-04 16:04:28 -0600

    Updated discover cards

--- roommate-finder/src/components/Discover.js
+++ roommate-finder/src/components/Discover.js
@@ -1,18 +1,20 @@
 import React from 'react';
+import {useState, useEffect} from 'react';
+import DiscoverCard from './DiscoverCard';
 
 export default function Discover() {
 
-    const [users, setUsers] = useState(null);
+    const [users, setUsers] = useState([]);
 
     useEffect(() => {
         setUsers()
-    })
+    }, [])
 
     return (
         <div>
             <div>
                 {users.map((u) => (
-                    <DiscoverCard />
+                    <DiscoverCard key={u} name={u}/>
                 ))}
             </div>
         </div>
--- roommate-finder/src/components/DiscoverCard.js
+++ roommate-finder/src/components/DiscoverCard.js
@@ -1,20 +1,20 @@
-import React from 'React';
+import React from 'react';
 
 export default function DiscoverCard(props) {
 
      let imageURL; 
 
      return (
-          <div class="card mb-3" style="max-width: 540px;">
-               <div class="row g-0">
-                    <div class="col-md-4">
-                          <img src="..." class="img-fluid rounded-start" alt="..." />
+          <div className="card mb-3">
+               <div className="row g-0">
+                    <div className="col-md-4">
+                          <img src="" className="img-fluid rounded-start" alt="..." />
                     </div>
-                    <div class="col-md-8">
-                         <div class="card-body">
-                              <h5 class="card-title">Chase Mathis</h5>
-                              <p class="card-text"></p>
-                              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <div className="col-md-8">
+                         <div className="card-body">
+                              <h5 className="card-title">{props.name}</h5>
+                              <p className="card-text"></p>
+                              <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
                          </div>
                     </div>
                </div>
