f555740caea1709dd4c8b4bc01649fadf2cb39d6
JasonLin3 <jasonlin8812@gmail.com>
2023-03-04 16:26:42 -0600

    Fixed flask server that calls backend query.py to get data from DB

--- backend/runBackend.sh
+++ backend/runBackend.sh
@@ -1,2 +1,3 @@
+pip3 install -r requirements.txt
 FLASK_APP=server
 flask run
--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
+from query import get_user
+import sqlite3
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
+@app.route("/request1", methods=['GET'])
+def request1():
+    return get_user()
 
 if __name__== "__main__":
     app.run()
