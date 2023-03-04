d5f3187ec9ff08fc6fcd24abc154ce8aae50dece
Chase Mathis <chasemathis@Chases-MacBook-Air.local>
2023-03-04 16:42:41 -0600

    added onto Discover and DiscoverCard

--- roommate-finder/src/components/Discover.js
+++ roommate-finder/src/components/Discover.js
@@ -4,17 +4,45 @@ import DiscoverCard from './DiscoverCard';
 
 export default function Discover() {
 
-    const [users, setUsers] = useState([]);
+    const [users, setUsers] = useState([{
+        user_id: 0,
+        first_name: "Chase",
+        last_name: "Mathis",
+        age: 20,
+        school: "University of Wisconsin - Madison",
+        city: "Madison",
+        state: "Wisconsin",
+        min_price: 1000,
+        max_price: 2000
+    }, {
+        user_id: 1,
+        first_name: "Chris",
+        last_name: "Plagge",
+        age: 21,
+        school: "University of Wisconsin - Madison",
+        city: "Madison",
+        state: "Wisconsin",
+        min_price: 1500,
+        max_price: 2500
+    }]);
 
     useEffect(() => {
-        setUsers()
     }, [])
 
     return (
         <div>
             <div>
                 {users.map((u) => (
-                    <DiscoverCard key={u} name={u}/>
+                    <DiscoverCard 
+                        key={u.user_id}
+                        first_name={u.first_name}
+                        last_name={u.last_name}
+                        age={u.age}
+                        school={u.school}
+                        city={u.city}
+                        state={u.state}
+                        min_price={u.min_price}
+                        max_price={u.max_price}/>
                 ))}
             </div>
         </div>
