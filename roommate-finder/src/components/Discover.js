550f6863cbcd52e1adec7be8b1153a6d069f1ffd
Chase Mathis <chasemathis@Chases-MacBook-Air.local>
2023-03-04 15:17:51 -0600

    Created the Discover and DiscoverCard components

---
+++ roommate-finder/src/components/Discover.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Discover() {
+
+    const [users, setUsers] = useState(null);
+
+    useEffect(() => {
+        setUsers()
+    })
+
+    return
+        <div>
+            <div>
+                {users.map((u) =>)}
+            </div>
+        </div>
+}
