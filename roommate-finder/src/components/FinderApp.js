a64c4445bb0c01da7a6d485f5d7c7c2d91ceb9ac
cplagge2 <cplagge@wisc.edu>
2023-03-04 15:46:35 -0600

    Added navbar and pages

--- roommate-finder/src/components/FinderApp.js
+++ roommate-finder/src/components/FinderApp.js
@@ -2,12 +2,15 @@ import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import FinderLayout from './FinderLayout';
 import Discover from './Discover';
+import Profile from './Profile';
 
 function FinderApp(props) {
     return <BrowserRouter>
         <Routes>
             <Route path="/" element={<FinderLayout />}></Route>
+            <Route index element={<Discover></Discover>}></Route>
             <Route path="/discover" element={<Discover />}></Route>
+            <Route path="/profile" element={<Profile />}></Route>
         </Routes>
     </BrowserRouter>
 }
