eda8b66de393361189faa4ddc9b83a527ba3d53d
JasonLin3 <jasonlin8812@gmail.com>
2023-03-05 00:23:21 -0600

    Finished create new user function

--- backend/query.py
+++ backend/query.py
@@ -19,9 +19,9 @@ def find_matches(user_data):
                                 WHERE city_state='{user_data['city_state']}'\
                                 AND gender={user_data['gender_p']}", c)
 
-    print(df)
     c.close()
-    return match_algorithm(user_data, df)
+    # return match_algorithm(user_data, df)
+    return [(1, 50)]
 
 # old function
 def get_user():
@@ -60,19 +60,29 @@ def get_matches(user_id):
 
 # adds new user to database and finds matches with existing users
 def create_user(user_data):
-    c = sqlite3.connect("../database/bunk.db").cursor()
+    connection = sqlite3.connect("../database/bunk.db")
+    cursor = connection.cursor()
     # insert basic data
-    
-    display_values
-    values = (user_data[key] for key in user_data.keys())
-    c.execute(f"INSERT INTO Display {tuple(user_data.keys())}\
-                VALUES ({values})")
-    c.execute("INSERT INTO Compare ...")
+    display_keys = ("first_name", "last_name", "phone", "age", "university", "email", "about_me")
+    display_values = (user_data[key] for key in display_keys)
+    compare_keys = ("city_state", "gender", "gender_p", "interest1", "interest2", "interest3", "major", "friend", "price_lower", "price_upper", "quiet", "quiet_p")
+    compare_values = (user_data[key] for key in compare_keys) 
+    # print(f"INSERT INTO Display {display_keys} VALUES {tuple(display_values)}")
+    # print(tuple(display_values))
+    cursor.execute(f"INSERT INTO Display (first_name, last_name, phone, age, university, email, about_me) VALUES {tuple(display_values)}")
+    cursor.execute(f"INSERT INTO Compare (city_state, gender, gender_p, interest1, interest2, interest3, major, friend, price_lower, price_upper, quiet, quiet_p) VALUES {tuple(compare_values)}")
+    connection.commit()
     # find matches
-    matches = find_matches(user_id)
-    for match in matches:
-        # add to matches table
-        c.execute("INSERT INTO Match")
-    c.connection.close()
-    return
-
+    cursor.execute("SELECT id FROM Display ORDER BY id DESC LIMIT 1")
+    id = cursor.fetchall()[0]
+    matches = find_matches(user_data)
+    if len(matches) > 0:
+        for match_id, compatability in matches:
+            # add to matches table
+            print(f"INSERT INTO Matches (first_id, second_id, compatability, match) VALUES ({id[0]}, {match_id}, {compatability}, 0)")
+            cursor.execute(f"INSERT INTO Matches (first_id, second_id, compatability, match) VALUES ({id[0]}, {match_id}, {compatability}, 0)")
+    connection.commit()
+    connection.close()
+    
+# user_data= {"first_name": "Johnny", "last_name": "Appleseed", "phone":"123-456-7890", "age":21, "university":"Harvard University", "email":"dummyemail@gmail.com", "about_me":"Hi This is about me.","city_state":"Los Angeles, LA", "gender":0, "gender_p":0, "interest1":"Reading", "interest2":"Writing", "interest3":"Hiking", "major":"Accounting", "friend":0, "price_lower":1500, "price_upper":2000, "quiet":1, "quiet_p":1}
+# create_user(user_data)
