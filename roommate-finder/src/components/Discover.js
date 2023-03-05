ceffabef58c01670a79e8aaf936fd5f512613a2f
Chase Mathis <chasemathis@Chases-MacBook-Air.local>
2023-03-04 18:01:14 -0600

    Worked on Discovery and DiscoveryCard

--- roommate-finder/src/components/Discover.js
+++ roommate-finder/src/components/Discover.js
@@ -5,27 +5,27 @@ import DiscoverCard from './DiscoverCard';
 export default function Discover() {
 
     const [users, setUsers] = useState([{
-        user_id: 0,
+        id: 0,
         first_name: "Chase",
         last_name: "Mathis",
         age: 20,
-        gender: "Male",
+        gender: 0,
         school: "University of Wisconsin - Madison",
         city: "Madison",
         state: "Wisconsin",
-        min_price: 1000,
-        max_price: 2000
+        price_lower: 1500,
+        price_upper: 2500
     }, {
-        user_id: 1,
+        id: 1,
         first_name: "Chris",
         last_name: "Plagge",
-        gender: "Male",
+        gender: 0,
         age: 21,
         school: "University of Wisconsin - Madison",
         city: "Madison",
         state: "Wisconsin",
-        min_price: 1500,
-        max_price: 2500
+        price_lower: 1500,
+        price_upper: 2500
     }]);
 
     useEffect(() => {
@@ -36,7 +36,7 @@ export default function Discover() {
             <div>
                 {users.map((u) => (
                     <DiscoverCard 
-                        key={u.user_id}
+                        key={u.id}
                         first_name={u.first_name}
                         last_name={u.last_name}
                         gender={u.gender}
@@ -44,8 +44,8 @@ export default function Discover() {
                         school={u.school}
                         city={u.city}
                         state={u.state}
-                        min_price={u.min_price}
-                        max_price={u.max_price}/>
+                        price_lower={u.price_lower}
+                        price_upper={u.price_upper}/>
                 ))}
             </div>
         </div>
