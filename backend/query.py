ac821c79dfa754d23fd16c2982faf2852f2cd147
JasonLin3 <jasonlin8812@gmail.com>
2023-03-05 09:32:11 -0600

    Fixed database death by commiting after every update

--- backend/query.py
+++ backend/query.py
@@ -69,13 +69,15 @@ def find_matches(user_data):
     if user_data['gender_p'] == 2:
         df = pd.read_sql_query(f"SELECT * \
                             FROM Compare\
-                            WHERE city_state='{user_data['city_state']}'\
+                            WHERE id!={user_data['id']}\
+                            city_state='{user_data['city_state']}'\
                             AND full_time='{user_data['full_time']}'", c)
         
     else:
         df = pd.read_sql_query(f"SELECT * \
                                 FROM Compare\
-                                WHERE city_state='{user_data['city_state']}'\
+                                WHERE id!={user_data['id']}\
+                                city_state='{user_data['city_state']}'\
                                 AND gender='{user_data['gender_p']}'\
                                 AND full_time='{user_data['full_time']}'", c)
     print(df)
@@ -107,9 +109,9 @@ def get_discover(email):
 
     # get user_ids of matches
     match_ids = []
-    c.execute(f"SELECT first_id FROM Matches WHERE second_id={user_id}")
+    c.execute(f"SELECT first_id FROM Matches WHERE second_id={user_id} AND match=1")
     match_ids.extend(c.fetchall())
-    c.execute(f"SELECT second_id FROM Matches WHERE first_id={user_id}")
+    c.execute(f"SELECT second_id FROM Matches WHERE first_id={user_id} AND match=2")
     match_ids.extend(c.fetchall())
     # get info about matches
     # id, first_name, last_name, gender, age, major, city_state, price_upper, price_lower, interests, about_me
@@ -119,13 +121,46 @@ def get_discover(email):
         c.execute(f"SELECT D.id, D.first_name, D.last_name, C.gender, D.age, C.major, D.university, C.city_state, C.price_upper, C.price_lower, D.about_me, C.interest1, C.interest2, C.interest3\
              FROM Display D, Compare C\
              WHERE D.id = C.id\
-             AND D.id={match_id[0]}")
+             AND D.id={match_id[0]}\
+             ORDER BY compatability DESC")
         # print(dict(zip(keys,c.fetchall()[0])))
         matches.append(dict(zip(keys,c.fetchall()[0])))
     c.connection.close()
     return matches
 
-get_discover("dummyemail@gmail.com")
+# get_discover("dummyemail@gmail.com")
+
+def get_matches(email):
+    c = sqlite3.connect("../database/bunk.db").cursor()
+
+    # get user_id
+    c.execute(f"SELECT id FROM Display WHERE email='{email}'")
+    result = c.fetchone()
+    if result:
+        user_id = result[0]
+    else:
+        return
+
+    # get user_ids of matches
+    match_ids = []
+    c.execute(f"SELECT first_id FROM Matches WHERE second_id={user_id} AND match=3")
+    match_ids.extend(c.fetchall())
+    c.execute(f"SELECT second_id FROM Matches WHERE first_id={user_id} AND match=3")
+    match_ids.extend(c.fetchall())
+    # get info about matches
+    # id, first_name, last_name, gender, age, major, city_state, price_upper, price_lower, interests, about_me
+    matches = []
+    keys = ["id", "first_name", "last_name", "gender", "age", "major", "university", "city_state", "price_upper", "price_lower", "about_me", "interest1", "interest2", "interest3", "full_time"]
+    for match_id in match_ids:
+        c.execute(f"SELECT D.id, D.first_name, D.last_name, C.gender, D.age, C.major, D.university, C.city_state, C.price_upper, C.price_lower, D.about_me, C.interest1, C.interest2, C.interest3\
+             FROM Display D, Compare C\
+             WHERE D.id = C.id\
+             AND D.id={match_id[0]}\
+             ORDER BY compatability DESC")
+        # print(dict(zip(keys,c.fetchall()[0])))
+        matches.append(dict(zip(keys,c.fetchall()[0])))
+    c.connection.close()
+    return matches
 
 # adds new user to database and finds matches with existing users
 def create_user(user_data):
@@ -135,14 +170,16 @@ def create_user(user_data):
     id = cursor.fetchall()[0][0]+1
     print(id)
     # insert basic data
-    display_keys = ("id", "first_name", "last_name", "phone", "age", "university", "email", "about_me")
+    display_keys = ("id", "first_name", "last_name", "phone", "age", "university", "email", "about_me", "img_link")
     user_data["id"] = id
     display_values = (user_data[key] for key in display_keys)
     compare_keys = ("id", "city_state", "full_time", "gender", "gender_p", "interest1", "interest2", "interest3", "major", "friend", "price_lower", "price_upper", "quiet", "quiet_p")
     compare_values = (user_data[key] for key in compare_keys)
     # print(f"INSERT INTO Display {display_keys} VALUES {tuple(display_values)}")
-    cursor.execute(f"INSERT INTO Display (id, first_name, last_name, phone, age, university, email, about_me) VALUES {tuple(display_values)}")
+    cursor.execute(f"INSERT INTO Display (id, first_name, last_name, phone, age, university, email, about_me, img_link) VALUES {tuple(display_values)}")
+    connection.commit()
     cursor.execute(f"INSERT INTO Compare (id, city_state, full_time, gender, gender_p, interest1, interest2, interest3, major, friend, price_lower, price_upper, quiet, quiet_p) VALUES {tuple(compare_values)}")
+    connection.commit()
     # connection.commit()
     # find matches
     matches = find_matches(user_data)
@@ -151,9 +188,58 @@ def create_user(user_data):
             # add to matches table
             print(f"INSERT INTO Matches (first_id, second_id, compatability, match) VALUES ({id}, {match_id}, {compatability}, 0)")
             cursor.execute(f"INSERT INTO Matches (first_id, second_id, compatability, match) VALUES ({id}, {match_id}, {compatability}, 0)")
-    connection.commit()
+            connection.commit()
     connection.close()
     return "NICE"
     
 # user_data= {"first_name": "Johnny", "last_name": "Appleseed","full_time":1, "phone":"123-456-7890", "age":21, "university":"Harvard University", "email":"dummyemail@gmail.com", "about_me":"Hi This is about me.","city_state":"Los Angeles, CA", "gender":0, "gender_p":0, "interest1":"Reading", "interest2":"Writing", "interest3":"Hiking", "major":"Accounting", "friend":0, "price_lower":1500, "price_upper":2000, "quiet":1, "quiet_p":1}
 # create_user(user_data)
+
+def update_match(email1, id2, match_state):
+    connection = sqlite3.connect("../database/bunk.db")
+    cursor = connection.cursor()
+
+    # get user id
+    cursor.execute(f"SELECT id from Display WHERE email='{email1}' LIMIT 1")
+    id1 = cursor.fetchone()
+    if id1:
+        id1 = id1[0]
+
+    # get prev state
+    ## if first id - the one being updated 
+    cursor.execute(f"SELECT match FROM Matches WHERE (first_id={id1} AND second_id={id2})")
+    match = cursor.fetchone()
+    if match:
+        id1 = id1[0]
+        first = True
+    else:
+        return "No Match Found"
+    
+    ##second id is being updated 
+    cursor.execute(f"SELECT match FROM Matches WHERE (first_id={id2} AND second_id={id1})")
+    match = cursor.fetchone()
+    if match:
+        id1 = id1[0]
+        first = False
+    else:
+        return "No Match Found"
+    
+    if (match == 1 or match == 2) and match_state == 1:
+        match = 3
+    elif match == 0 and match_state == 1:
+        if first:
+            ## first id 
+            match = 1
+        else:
+            ##second id 
+            match = 2
+    elif match_state == 0:
+        match = 4
+
+    if first:
+        cursor.execute(f"UPDATE Matches SET match={match} WHERE first_id={id1} AND second_id={id2}")
+    else:
+        cursor.execute(f"UPDATE Matches SET match={match} WHERE first_id={id2} AND second_id={id1}")
+    connection.commit()
+    connection.close()
+    return "Match updated probably"
--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from query import get_user, get_discover, create_user
+from query import get_user, get_discover, get_matches, create_user, update_match
 import sqlite3
 from flask_cors import CORS
 
@@ -24,10 +24,24 @@ def get_discover_request():
     return jsonify(get_discover(email))
 
 
+@app.route("/getMatches", methods=['GET'])
+def get_matches_request():
+    email = request.args.get('email')
+    return jsonify(get_matches(email))
+
+
 @app.route("/createUser", methods=['POST'])
 def create_user_request():
     user_data = request.json
     return create_user(user_data)
+
+@app.route("/updateMatch", methods=['POST'])
+def update_match_request():
+    user_data = request.json
+    email = user_data['email']
+    id = user_data['id']
+    match_state = user_data['match_state']
+    return update_match(email, id, match_state)
 
 if __name__== "__main__":
     app.run()
