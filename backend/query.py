e7724b7789414d70e33f7a0ca63990b47857b2b7
JasonLin3 <jasonlin8812@gmail.com>
2023-03-04 21:58:40 -0600

    Added new queries for new user and get matches

--- backend/query.py
+++ backend/query.py
@@ -2,6 +2,11 @@ import sqlite3
 from flask import jsonify
 
 
+# algorithm 
+def find_matches():
+    pass
+
+# old function
 def get_user():
     c = sqlite3.connect("../database/bunk.db").cursor()
     c.execute("SELECT * FROM User")
@@ -10,3 +15,43 @@ def get_user():
     keys = ["id", "first_name", "last_name", "age", "gender", "price_lower", "price_upper", "friend", "quiet"]
     users = [dict(zip(keys, d)) for d in data]
     return jsonify(users)
+
+# gets matches for a particular user id
+def get_matches(user_id):
+    c = sqlite3.connect("../database/bunk.db").cursor()
+
+    # get user_ids of matches
+    match_ids = []
+    c.execute("SELECT first_id FROM Matches WHERE second_id={user_id}")
+    match_ids.append(c.fetchall())
+    c.execute("SELECT second_id FROM Matches WHERE first_id={user_id}")
+    match_ids.append(c.fetchall())
+
+    # get info about matches
+    # id, first_name, last_name, gender, age, major, city_state, price_upper, price_lower, interests, about_me
+    matches = []
+    keys = ["id", "first_name", "last_name", "gender", "age", "major", "city_state", "price_upper", "price_lower", "about_me"]
+    for match_id in match_ids:
+        c.execute(
+            "SELECT id, first_name, last_name, gender, age, major, city_state, price_upper, price_lower, about_me\
+             FROM Display D, Compare C\
+             WHERE D.id = C.id\
+             AND id={match_id}")
+        matches.append(dict(zip(keys,c.fetchall())))
+    c.connection.close()
+    return jsonify(matches)
+
+# adds new user to database and finds matches with existing users
+def create_user(user_data):
+    c = sqlite3.connect("../database/bunk.db").cursor()
+    # insert basic data
+    c.execute("INSERT INTO Display ...")
+    c.execute("INSERT INTO Compare ...")
+    # find matches
+    matches = find_matches(user_id)
+    for match in matches:
+        # add to matches table
+        c.execute("INSERT INTO Match")
+    c.connection.close()
+    return
+
--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,5 @@
-from flask import Flask, jsonify
-from query import get_user
-import sqlite3
+from flask import Flask, request
+from query import get_user, get_matches, create_user
 
 app = Flask(__name__)
 
@@ -8,9 +7,22 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+
 @app.route("/request1", methods=['GET'])
 def request1():
     return get_user()
+
+
+@app.route("/getMatches", methods=['GET'])
+def get_matches_request():
+    user_id = request.args.get('user_id')
+    return get_matches(user_id)
+
+
+@app.route("/createUser", methods=['POST'])
+def create_user_request():
+    user_data = request.json
+    return create_user(user_data)
 
 if __name__== "__main__":
     app.run()
